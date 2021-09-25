906c7a7451ce1882432f5262379131aa4683fb8d
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-25 21:42:46 +0200

    stuff

--- packages/client/views/Lobby.tsx
+++ packages/client/views/Lobby.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
 
 export default function Lobby() {
   return (
     <div className="shadow-xl bg-white p-8 rounded-xl w-full	">
-      <h1>Waiting for other players</h1>
+      <h1>Matching you with other players...</h1>
       <div className=" transform -translate-x-5">
-        <div className="m-10 flex justify-center items-center fill-current bg-red-600 animate-spin h-5 w-5 mr-3">
-          <svg className="" viewBox="0 0 50 50"></svg>
+        <div className="m-10 flex justify-center items-center fill-current animate-spin ">
+          <AiOutlineLoading style={{ width: 50, height: 50 }} />
         </div>
       </div>
     </div>
--- packages/client/views/Question.tsx
+++ packages/client/views/Question.tsx
@@ -4,11 +4,11 @@ import { ProgressBar } from "../components/ProgressBar";
 
 export default function Question() {
   const [answer, setAnswer] = useState("");
+  const [value, setValue] = useState("X Y Z and then XYZ");
   const disabled = !answer;
   return (
     <div className="shadow-xl bg-white p-8 rounded-xl w-full	">
       <ProgressBar progressPercentage={80} isDanger={false} />
-
       <h3 className="text-gray-600 pt-8">
         How would you describe the following word?
       </h3>
