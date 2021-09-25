edeab22cba4d046619df1f69011f6152f0f27f8b
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-25 14:56:09 +0200

    Q template

--- packages/client/components/Question.tsx
+++ packages/client/components/Question.tsx
@@ -3,11 +3,11 @@ import { Button } from "./Button";
 
 export default function Question() {
   return (
-    <div>
-      <h3>Describe this word</h3>
+    <div className="shadow-lg rounded-xl bg-blue-400 p-8">
       <h2>30 seconds left</h2>
+      <h3>How would you describe the following word?</h3>
       <h2>Sprechen</h2>
-      <input type="text" name="description"></input>
+      <input type="text" name="description" className="w-full"></input>
       <Button>Submit</Button>
     </div>
   );
