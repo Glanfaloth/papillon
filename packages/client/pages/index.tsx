61b556e5125f98025ff5d19f5e6f276972e3e011
Yelan Tao <yeltao@student.ethz.ch>
2021-09-25 14:44:17 +0200

    waiting to lobby

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Homepage from "../components/Homepage";
-import Waiting from "../components/Waiting";
+import Lobby from "../components/Lobby";
 import Question from "../components/Question";
 import Choice from "../components/Choice";
 import Score from "../components/Scores";
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <Homepage />
-      <Waiting />
+      <Lobby />
       <Question />
       <Choice />
       <Score />
