c5dab5c6d015657f311195707aa9dbe3e92dad7b
Michael Giger <migiger@student.ethz.ch>
2021-09-25 17:25:11 +0200

    Added individual colors to bar chart

--- packages/client/views/Scores.tsx
+++ packages/client/views/Scores.tsx
@@ -32,21 +32,29 @@ const data = [
   },
 ];
 
+const bar_color = ["#cb4335", "#28b463", "#3498db", "#f1c40f"]
+
 export default function Scores() {
   const sortedData = _.sortBy(data, (o: typeof data[0]) => -1 * o.points);
   return (
-    <div className="bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 overflow-hidden">
-      <h1>Scores</h1>
-      <BarChart width={300} height={100} data={sortedData}>
-        <Bar dataKey="points" fill="white" />
-        <XAxis
-          dataKey="name"
-          tick={{ stroke: "white", fill: "white" }}
-          axisLine={false}
-          tickLine={false}
-        />
-      </BarChart>
-      <h1>Done</h1>
+
+    <div className="bg-gradient-to-r from-green-200 to-blue-300 overflow-hidden">
+      <div className="shadow-lg rounded-xl bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 p-8">
+        <h1>Scores</h1>
+        <BarChart width={300} height={100} data={sortedData}>
+          <Bar dataKey="points">
+            {data.map((entry, index) => (
+              <Cell fill={bar_color[index % 4]} key={`cell-${index}`} />
+            ))}
+          </Bar>
+          <XAxis
+            dataKey="name"
+            tick={{ stroke: "white", fill: "white" }}
+            axisLine={false}
+            tickLine={false}
+          />
+        </BarChart>
+      </div>
     </div>
   );
 }
