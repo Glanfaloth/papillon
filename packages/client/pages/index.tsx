ff3049ea6ce771cbc55a60e9cd7f67a090f0fd4e
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-25 22:54:38 +0200

    steps

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -12,17 +12,24 @@ export default function Home() {
 
   useEffect(() => {
     console.log("state");
+    console.log("Step is", state?.step);
 
     console.log(state);
   }, [state]);
 
+  const step = state?.step;
+
   return (
     <div className="space-y-10 bg-gradient-to-r to-pink-600 from-blue-700 w-screen h-full flex-row p-20">
-      <Homepage />
-      <Lobby />
+      {step?.type === undefined && <Homepage />}
+      {step?.type === "waiting" && <Lobby />}
+      {step?.type === "write-description" && <Question />}
+      {step?.type === "choose-word" && <Choice />}
+      {step?.type === "end-screen" && <Scores />}
+      {/* <Lobby />
       <Question />
       <Choice />
-      <Scores />
+      <Scores /> */}
     </div>
   );
 }
