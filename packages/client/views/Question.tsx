7a487e301502b90c2335e7848fdd5f327aad08db
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-25 15:04:09 +0200

    progress bar

--- packages/client/views/Question.tsx
+++ packages/client/views/Question.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import { Button } from "../components/Button";
 
+interface ProgressBarProps {
+  progressPercentage: number;
+  isDanger: boolean;
+}
+
+const ProgressBar = ({ progressPercentage, isDanger }: ProgressBarProps) => {
+  return (
+    <div className="h-8 md:h-6 w-full bg-gray-300 justify-start flex-row overflow-hidden">
+      <div
+        style={{ width: `${progressPercentage}%` }}
+        className={`h-full bg-gradient-to-r${
+          isDanger
+            ? " from-purple-400 via-pink-500 to-red-500"
+            : " from-blue-500 to-green-400"
+        }`}
+      />
+    </div>
+  );
+};
+
 export default function Question() {
   return (
-    <div className="shadow-lg rounded-xl bg-blue-400 p-8">
+    <div className="shadow-lg rounded-xl bg-gradient-to-r from-green-200 to-blue-300 p-8">
+      <ProgressBar progressPercentage={80} isDanger={false} />
       <h2>30 seconds left</h2>
       <h3>How would you describe the following word?</h3>
       <h2>Sprechen</h2>
