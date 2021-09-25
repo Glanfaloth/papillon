10255c1656845928f98f73b9a318b7c03e7e40b0
Yelan Tao <yeltao@student.ethz.ch>
2021-09-25 20:59:42 +0200

    highlight textarea

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { initActivityRoom } from "../colyseus/activity-room";
 import "../styles/global.css";
+import "../styles/highlight.css";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
--- packages/client/views/Question.tsx
+++ packages/client/views/Question.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Button, ButtonVariant } from "../components/Button";
 import { ProgressBar } from "../components/ProgressBar";
 
 export default function Question() {
   const [answer, setAnswer] = useState("");
   const disabled = !answer;
+
+  function applyHighlights(text: string ) {
+    text = text
+      .replace(/\n$/g, "\n\n")
+      .replace(/[A-Z].*?\b/g, "<mark>$&</mark>");
+    return text;
+  }
+
   return (
     <div className="shadow-xl bg-white p-8 rounded-xl w-full	">
       <ProgressBar progressPercentage={80} isDanger={false} />
@@ -13,12 +21,22 @@ export default function Question() {
         How would you describe the following word?
       </h3>
       <h1>Sprechen</h1>
-      <textarea
-        className="border-gray-400 border-2 w-full"
-        name="description"
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-      />
+      <div className="container">
+        <div className="backdrop" >
+          <span
+          dangerouslySetInnerHTML={{ __html: applyHighlights(answer)}}
+            className="highlights"
+          ></span>
+        </div>
+        <textarea
+          className="border-gray-400 border-2 w-full"
+          name="description"
+          value={answer}
+          onChange={(e) => {
+            setAnswer(e.target.value);
+          }}
+        ></textarea>
+      </div>
       <Button
         variant={disabled ? ButtonVariant.SECONDARY : ButtonVariant.PRIMARY}
         onClick={() => {
