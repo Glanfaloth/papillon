78f813052811394439bb8c943ba1286da5aaae69
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-25 22:09:05 +0200

    score numbers

--- packages/client/views/Scores.tsx
+++ packages/client/views/Scores.tsx
@@ -8,7 +8,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer
+  ResponsiveContainer,
 } from "recharts";
 import _ from "lodash";
 import { GiWhiteBook } from "react-icons/gi";
@@ -16,20 +16,20 @@ import { GiWhiteBook } from "react-icons/gi";
 const data = [
   {
     name: "Tomato",
-    points: 100
+    points: 10,
   },
   {
     name: "Pesca",
-    points: 232
+    points: 22,
   },
   {
     name: "Kiwi",
-    points: 400
+    points: 12,
   },
   {
     name: "Fruit",
-    points: 200
-  }
+    points: 3,
+  },
 ];
 
 const bar_color = ["#cb4335", "#28b463", "#3498db", "#f1c40f"];
@@ -40,11 +40,19 @@ export default function Scores() {
     <div className="shadow-xl bg-white p-8 rounded-xl w-full	">
       <h1>Scores</h1>
       <BarChart width={600} height={200} data={sortedData}>
-        <Bar dataKey="points">
+        <Bar dataKey="points" label={{ position: "top" }}>
           {data.map((entry, index) => (
             <Cell fill={bar_color[index % 4]} key={`cell-${index}`} />
           ))}
         </Bar>
+        <YAxis
+          type="number"
+          domain={[0, "dataMax"]}
+          padding={{ top: 15 }}
+          tickLine={false}
+          axisLine={false}
+          tick={false}
+        />
         <XAxis
           dataKey="name"
           tick={{ stroke: "black", fill: "black" }}
