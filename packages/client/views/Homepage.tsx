0c559adaf95fcec5be9448b913fc20fc8ef6a7dd
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-25 18:36:06 +0200

    styling fix

--- packages/client/views/Homepage.tsx
+++ packages/client/views/Homepage.tsx
@@ -6,18 +6,18 @@ export default function Homepage() {
   return (
     <div className="bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 overflow-hidden">
       <div className="space-y-10">
-      <span className="inline-grid grid-cols-3 gap-x-4 items-center">
-        <span>
-          <GiButterfly className="float-right text-white" />
+        <span className="inline-grid grid-cols-3 gap-x-2 items-center">
+          <span>
+            <GiButterfly className="float-right text-white" />
+          </span>
+          <h1 className="text-white">Welcome to Papillon</h1>
+          <span>
+            <GiButterfly className="float-left text-white" />
+          </span>
         </span>
-        <h1 className="text-white">Welcome to Papillon</h1>
-        <span>
-          <GiButterfly className="float-left text-white" />
-        </span>
-      </span>
-      <p className="text-white">What is your name?</p>
-      <input type="text"></input>
-      <Button onClick={() => {}}>Enter the Lobby</Button>
+        <h3 className="text-white pb-0">What is your name?</h3>
+        <input type="text"></input>
+        <Button onClick={() => {}}>Enter the Lobby</Button>
       </div>
     </div>
   );
