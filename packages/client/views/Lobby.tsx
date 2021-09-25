d86af957044661f18d66ad887798cdc0eb80bf29
Michael Giger <migiger@student.ethz.ch>
2021-09-25 21:39:40 +0200

    Add loading animation

--- packages/client/views/Lobby.tsx
+++ packages/client/views/Lobby.tsx
@@ -5,8 +5,10 @@ export default function Lobby() {
     <div className="shadow-xl bg-white p-8 rounded-xl w-full	">
       <h1>Waiting for other players</h1>
       <div className=" transform -translate-x-5">
-        <div className="m-10 flex justify-center items-center fill-current bg-red-600 animate-spin h-5 w-5 mr-3">
-          <svg className="" viewBox="0 0 50 50"></svg>
+        <div className="m-10 flex-row space-x-3 justify-center items-center mr-0">
+          <div className="w-3 h-3 bg-black animate-rotate0"></div>
+          <div className="w-3 h-3 bg-black animate-rotate1"></div>
+          <div className="w-3 h-3 bg-black animate-rotate2"></div>
         </div>
       </div>
     </div>
