60d40a69754679802d02fc5e24e4064478d9c15e
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-25 16:51:08 +0200

    spinning

--- packages/client/views/Lobby.tsx
+++ packages/client/views/Lobby.tsx
@@ -25,7 +25,9 @@ export default function Lobby() {
   return (
     <div>
       <h1>Waiting for other players...</h1>
-      <VscLoading />
+      <div className="fill-current bg-red-600  animate-spin h-5 w-5 mr-3">
+        <svg className="" viewBox="0 0 24 24"></svg>
+      </div>
     </div>
   );
 }
